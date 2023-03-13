a44dfbd8ab631785ae495795aa02d75b2c403fad
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-03-13 18:50:32 +0100

    Milestone 3 ok

--- js/main.js
+++ js/main.js
@@ -22,122 +22,6 @@ Inizialmente può essere sufficiente stampare dei semplici div, senza alcuno sti
 const provaDom = document.getElementById("cards-container");
 const tipoSeleione = document.getElementById('casellaSelezione');
 
-/*
-const oggetti = [
-    {
-        name: 'cat',
-        prefix: 'fa-',
-        type: 'animal',
-        family: 'fas',
-        color: 'orange'
-    },
-    {
-        name: 'crow',
-        prefix: 'fa-',
-        type: 'animal',
-        family: 'fas',
-        color: 'orange'
-    },
-    {
-        name: 'dog',
-        prefix: 'fa-',
-        type: 'animal',
-        family: 'fas',
-        color: 'orange'
-    },
-    {
-        name: 'dove',
-        prefix: 'fa-',
-        type: 'animal',
-        family: 'fas',
-        color: 'orange'
-    },
-    {
-        name: 'dragon',
-        prefix: 'fa-',
-        type: 'animal',
-        family: 'fas',
-        color: 'orange'
-    },
-    {
-        name: 'horse',
-        prefix: 'fa-',
-        type: 'animal',
-        family: 'fas',
-        color: 'orange'
-    },
-    {
-        name: 'hippo',
-        prefix: 'fa-',
-        type: 'animal',
-        family: 'fas',
-        color: 'orange'
-    },
-    {
-        name: 'fish',
-        prefix: 'fa-',
-        type: 'animal',
-        family: 'fas',
-        color: 'orange'
-    },
-    {
-        name: 'carrot',
-        prefix: 'fa-',
-        type: 'vegetable',
-        family: 'fas',
-        color: 'green'
-    },
-    {
-        name: 'apple-alt',
-        prefix: 'fa-',
-        type: 'vegetable',
-        family: 'fas',
-        color: 'green'
-    },
-    {
-        name: 'lemon',
-        prefix: 'fa-',
-        type: 'vegetable',
-        family: 'fas',
-        color: 'green'
-    },
-    {
-        name: 'pepper-hot',
-        prefix: 'fa-',
-        type: 'vegetable',
-        family: 'fas',
-        color: 'green'
-    },
-    {
-        name: 'user-astronaut',
-        prefix: 'fa-',
-        type: 'user',
-        family: 'fas',
-        color: 'blue'
-    },
-    {
-        name: 'user-graduate',
-        prefix: 'fa-',
-        type: 'user',
-        family: 'fas',
-        color: 'blue'
-    },
-    {
-        name: 'user-ninja',
-        prefix: 'fa-',
-        type: 'user',
-        family: 'fas',
-        color: 'blue'
-    },
-    {
-        name: 'user-secret',
-        prefix: 'fa-',
-        type: 'user',
-        family: 'fas',
-        color: 'blue'
-    }
-];
-*/
 stampoLemieCard();
 
 function stampoLemieCard(tipo) {
@@ -149,9 +33,7 @@ function stampoLemieCard(tipo) {
                             <div>${element.name}</div>
                             </div>`;
 
-
-        if (element.type == tipo)
-
+        if (element.type == tipo || tipo == "all")
             provaDom.innerHTML += imageCard;
     });
 }
@@ -162,10 +44,7 @@ tipoSeleione.addEventListener("change",
         let sceltaTipo = tipoSeleione.value;
         provaDom.innerHTML = "";
         stampoLemieCard(sceltaTipo)
-
-
     }
-
 )
 
 
