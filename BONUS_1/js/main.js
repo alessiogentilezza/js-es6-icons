0fa5841e4193b90d55058eef1ec0ed61d1f7fd1b
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-03-13 22:47:42 +0100

    inserita funzione per generare numeri casuali da 0 a 255 al posto del colore

--- BONUS_1/js/main.js
+++ BONUS_1/js/main.js
@@ -1,13 +1,4 @@
-﻿/*
-Milestone 1
-Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell’icona e l’icona stessa.
-
-Milestone 2
-Ciascuna icona ha una proprietà “color”: utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
-
-Milestone 3
-Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l’utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
-
+/*
 BONUS
 1- modificare la struttura dati fornita e valorizzare la proprietà “color” in modo dinamico: generare in modo casuale un codice colore, sapendo che la notazione esadecimale è formata dal simbolo “#” seguito da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F.
 
@@ -29,7 +20,7 @@ function stampoLemieCard(tipo) {
     oggetti.forEach(element => {
 
         let imageCard = `<div class="card">
-                            <i class="fa-solid ${element.prefix}${element.name}"style="color:${element.color}"></i>
+                            <i class="fa-solid ${element.prefix}${element.name}"style="color:rgb(${generaNumeriCasualiUnici(3, 255)})"></i>
                             <div>${element.name}</div>
                             </div>`;
 
@@ -46,3 +37,19 @@ tipoSeleione.addEventListener("change",
         stampoLemieCard(sceltaTipo)
     }
 )
+
+function generaNumeriCasualiUnici(quantitàDesiderata, valoreMassimo) {
+    let numeriUnici = [];
+
+    while (numeriUnici.length < quantitàDesiderata) {
+        const nuovoNumero = Math.floor(Math.random() * valoreMassimo) + 1;
+
+        if (!numeriUnici.includes(nuovoNumero)) {
+            numeriUnici.push(nuovoNumero);
+        }
+    }
+    return numeriUnici;
+}
+
+
+
